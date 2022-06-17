39758531763931f0eecf1507322e2ace203aa1c1
Isabella Reis <100042658+isabella-reis@users.noreply.github.com>
2022-06-17 12:26:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date() 
+    var hora = data.getHours() //pegando a hora local do PC;
+    msg.innerHTML = `Agora são ${hora} horas !! :)`
+    
+    if (hora >= 0 && hora < 12) { //Bom dia!
+        img.src = './imagens/manha.jpg'
+        document.body.style.background = '#a4b5c3'
+    } else if (hora >= 12 && hora < 18) { //Boa tarde! :I
+        img.src = './imagens/tarde.jpg'
+        document.body.style.background = '#c49e6e'
+    } else { //Boa noite! :(
+        img.src = './imagens/noite.jpg'
+        document.body.style.background = '#836184'
+    }
+}
+
