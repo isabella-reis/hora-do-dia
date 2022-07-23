b9a32021444875a789e916c995293c3481bf5b3a
Isabella Reis <100042658+isabella-reis@users.noreply.github.com>
2022-07-22 21:01:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function carregar() {
     msg.innerHTML = `Agora são ${hora} horas !! :)`
     
     if (hora >= 0 && hora < 12) { //Bom dia!
-        img.src = '.manha.jpg'
+        img.src = './manha.jpg'
         document.body.style.background = '#a4b5c3'
     } else if (hora >= 12 && hora < 18) { //Boa tarde! :I
-        img.src = '.tarde.jpg'
+        img.src = './tarde.jpg'
         document.body.style.background = '#c49e6e'
     } else { //Boa noite! :(
-        img.src = '.noite.jpg'
+        img.src = './noite.jpg'
         document.body.style.background = '#836184'
     }
 }
